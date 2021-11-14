63fa5accac88d34b995cc288e4df0bcf75dc2716
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2021-11-14 18:34:44 +0200

    Add project files.

---
+++ Core1/Entities/PortfolioItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Core1.Entities
+{
+    public class PortfolioItem : EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ Infrastructure/Datacontext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected overide void OnModelCreating(ModelBuilder model)
+        {
+            base.onMOdelCreatiog(modelBuilder);
+            modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValue("NEWID()")
+
+            modelBuilder.Entity<PortfolioItem>().Property(x=>x.Id).HasDefaultValue("NEWID()")
+        }
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<PortfolioItem> PortfolioItem { get; set; }
+        
+    }
+}
